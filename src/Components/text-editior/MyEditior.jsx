09f7c932851d1d1328aa1ee7fdcf5bc42300c06b
dipeshkumarsah98 <dipeshsah98@gmail.com>
2022-09-26 14:40:46 +0545

    adding text editor

--- src/Components/text-editior/MyEditior.jsx
+++ src/Components/text-editior/MyEditior.jsx
@@ -1,34 +1,67 @@
 import React, { useState } from "react";
-import { Editor, EditorState } from "draft-js";
-import "draft-js/dist/Draft.css";
+import { useRef } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const MyEditior = () => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
-  const editor = React.useRef(null);
-  function focusEditor() {
-    editor.current.focus();
-  }
+  const [value, setValue] = useState("");
+  const editor = useRef();
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
+  const handleSubmit = () => {
+    const data = editor.current.unprivilegedEditor.getHTML();
+    console.log(data);
+    setValue(data);
+  };
+
+  const getData = (data) => <div>{data}</div>;
   return (
-    <div
-      className="container my-10 border border-black h-52 cursor-text px-2 py-2"
-      onClick={focusEditor}
-    >
-      <Editor
+    <>
+      <ReactQuill
         ref={editor}
-        editorState={editorState}
-        onChange={setEditorState}
-        toolbar={{
-          inline: { inDropdown: true },
-          list: { inDropdown: true },
-          textAlign: { inDropdown: true },
-          link: { inDropdown: true },
-          history: { inDropdown: true },
-        }}
-        placeholder="Write something!"
-      />
-    </div>
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        className="h-96 py-5 mx-5 bg-gray-100 text-5xl"
+        placeholder="enter your text"
+      ></ReactQuill>
+      <button
+        className="bg-blue-600 text-white px-4 py-2 mt-20 ml-5"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+      <div className="mx-5 my-5">
+        {value && <div dangerouslySetInnerHTML={{ __html: value }}></div>}
+      </div>
+    </>
   );
 };
 
--- src/Pages/_postDetail.js
+++ src/Pages/_postDetail.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import Content from "../Components/postDetails/Content";
 import { Header } from "../Components/postDetails/Header";
 
 export const PostDetailPage = () => {
-  const { id } = useParams();
+  // const { id } = useParams();
   return (
     <div className="container px-3 md:px-0">
       <Header />
